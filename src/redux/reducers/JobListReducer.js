637f19faa6e57694ecffa107a0a1ef00a6327d98
Brittany Blake <brittany.blake49@gmail.com>
2020-10-28 13:31:12 +0100

    create reducer for joblist

---
+++ src/redux/reducers/JobListReducer.js
@@ -0,0 +1,38 @@
+const InitialState = {
+  loading: false,
+  data: [],
+  errorMsg: "",
+};
+
+const JobListReducer = (state = InitialState, action) => {
+  switch (action.type) {
+    case "JOB_LIST_LOADING":
+      return {
+        ...state,
+        loading: true,
+        data: [],
+        errorMsg: "",
+      };
+
+    case "JOB_LIST_FAIL":
+      return {
+        ...state,
+        loading: false,
+        data: [],
+        errorMsg: "Unable to get jobs",
+      };
+
+    case "JOB_LIST_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        data: action.payload,
+        count: action.payload.length,
+        errorMsg: "",
+      };
+    default:
+      return state;
+  }
+};
+
+export default JobListReducer;
