68b1f243b009d8657629ec822edf1a8effb897af
Brittany Blake <brittany.blake49@gmail.com>
2020-10-29 10:21:21 +0100

    add page number to temporarily solve api inernal error

--- src/redux/actions/JobActions.js
+++ src/redux/actions/JobActions.js
@@ -1,46 +1,48 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const GetJobList = () => async dispatch => {
+export const GetJobList = (page) => async (dispatch) => {
   try {
     dispatch({
-      type: 'JOB_LIST_LOADING',
+      type: "JOB_LIST_LOADING",
     });
 
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    const targetUrl = 'https://jobs.github.com/positions.json?';
+    const pageNum = page;
+
+    var proxyUrl = "https://cors-anywhere.herokuapp.com/",
+      targetUrl = `https://jobs.github.com/positions.json?page=${pageNum}`;
 
     const result = await axios.get(proxyUrl + targetUrl);
 
     dispatch({
-      type: 'JOB_LIST_SUCCESS',
+      type: "JOB_LIST_SUCCESS",
       payload: result.data,
     });
   } catch (e) {
     dispatch({
-      type: 'JOB_LIST_FAIL',
+      type: "JOB_LIST_FAIL",
     });
   }
 };
 
-export const GetJob = job => async dispatch => {
+export const GetJob = (job) => async (dispatch) => {
   try {
     dispatch({
-      type: 'JOB_LOADING',
+      type: "JOB_LOADING",
     });
 
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    const targetUrl = `https://jobs.github.com/positions/${job}.json?`;
+    var proxyUrl = "https://cors-anywhere.herokuapp.com/",
+      targetUrl = `https://jobs.github.com/positions/${job}.json?`;
 
     const result = await axios.get(proxyUrl + targetUrl);
 
     dispatch({
-      type: 'JOB_SUCCESS',
+      type: "JOB_SUCCESS",
       payload: result.data,
       jobId: job,
     });
   } catch (e) {
     dispatch({
-      type: 'JOB_FAIL',
+      type: "JOB_FAIL",
     });
   }
 };
