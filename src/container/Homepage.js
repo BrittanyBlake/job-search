7eb2277531f3680aa79600c3fc8b4eea5d1f2103
Brittany Blake <brittany.blake49@gmail.com>
2020-10-29 12:15:40 +0100

    add loading container

---
+++ src/container/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import JobList from '../components/JobList/JobList';
+
+function Homepage() {
+  return (
+    <div>
+      <JobList />
+    </div>
+  );
+}
+
+export default Homepage;
