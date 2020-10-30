0fce0025269da497816dbccbbae6736a4b2c7a4e
Brittany Blake <brittany.blake49@gmail.com>
2020-10-30 21:38:45 +0100

    add tests for reducers

---
+++ src/tests/reducers/JobListReducer.test.js
@@ -0,0 +1,18 @@
+import JobListReducer from '../../redux/reducers/JobListReducer'
+import * as reducerAction from "../../redux/actions/JobActions";
+
+describe("get Job List", () => {
+  const initialState = {
+    loading: false,
+    data: [],
+    errorMsg: "",
+  };
+
+  it("should return the initial state", () => {
+    expect(
+      JobListReducer(undefined, {
+        type: reducerAction.GetJobList,
+      })
+    ).toEqual(initialState);
+  });
+});
